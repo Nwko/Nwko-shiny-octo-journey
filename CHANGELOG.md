fb0abad4490e68f7da43a82fbdfe0df101e5f99c
Nox <83485955+Nwko@users.noreply.github.com>
2022-03-31 22:34:59 +0545

    +test#2
    Still just testing

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,6 @@
 - March 31st 2022 9:42PM
 - [+test]
 - I'm just testing nothing much
+- 10:34PM
+- [+test#2]
+- Still just testing
--- Deploy.sh
+++ Deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Navigate into the build output directory
-cd <build_dir>
+cd dist
 
 # Initialize a new Git repository
 git init
@@ -13,13 +13,13 @@ git init
 git add -A
 
 # Commit the changes with a message
-git commit -m "<commit_message>"
+git commit -m "Brrr"
 
 # Push the commit to the remote repo
-git push -f git@github.com:<username>/<repository>.git master:<branch_to_deploy>
+git push -f git@github.com:Nwko/soh.git master:gh-pages
 
 # Confirm the deployment has been successful
-echo "<comfirmation_message>"
+echo "Website go brrr"
 
 # Show the message for 3 seconds before closing
 sleep 3
